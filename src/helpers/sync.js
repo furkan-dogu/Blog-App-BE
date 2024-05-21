a1ce111f6345560b09189bfd9938fd732dc1f078
Furkan Doğu <furkandogu2018@gmail.com>
2024-05-21 21:27:19 +0300

    first commit

---
+++ src/helpers/sync.js
@@ -0,0 +1,11 @@
+"use strict"
+
+module.exports = async function() {
+
+    return null
+
+    const { mongoose } = require('../configs/dbConnection')
+    await mongoose.connection.dropDatabase()
+    console.log('- Database and all data DELETED!')
+
+}
