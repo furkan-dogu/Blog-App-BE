1bd208841ea00c9cbbc2a8b64cc241379ffe78cb
Furkan Doğu <furkandogu2018@gmail.com>
2024-06-10 23:28:02 +0300

    düzenleme

--- src/models/comment.js
+++ src/models/comment.js
@@ -8,14 +8,12 @@ const CommentSchema = new mongoose.Schema(
             type: mongoose.Schema.Types.ObjectId,
             ref: "User",
             required: true,
-            unique: true
         },
 
         blogId: {
             type: mongoose.Schema.Types.ObjectId,
             ref: "Blog",
             required: true,
-            unique: true
         },
 
         comment: {
